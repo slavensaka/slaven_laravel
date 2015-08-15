11919e9e3360e1eef0c68f4b6fb9fff3f6c09b51
Slaven Sakacic <slavensakacic@gmail.com>
2015-08-16 01:36:56 +0200

    new branch

--- resources/views/contactthanks.php
+++ resources/views/contactthanks.php
@@ -1,30 +1,14 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
-
 <head>
-	<title>A Nice & Simple Contact Form</title>
-	
+	<title>A Nice & Simple Contact Form</title>	
 	<link rel="stylesheet" type="text/css" href="style.css" />
 </head>
-
 <body>
-
 	<div id="page-wrap">
-
-		<img src="images/sys.png" alt="Android" />
-		
-			
-			
-		<br /><br />
-		
-			
-		<h1>Your message has been sent!</h1><br />
-		
-		<p><a href="index.php">Back to my site</a></p>
-	
+		<img src="images/sys.png" alt="Android" />	
+		<br /><br />		
+		<h1>Your message has been sent!</h1><br />		
+		<p><a href="index.php">Back to my site</a></p>	
 	</div>
-	
-	
-
 </body>
-
 </html>
