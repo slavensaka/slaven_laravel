f99ae493998ff92fa5867ce43eb064d1856f8435
slavensaka <slavensakacic@gmail.com>
2017-04-06 15:01:52 +0200

    New stuff

---
+++ app/Http/routes.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+
+	// \Mail::send('emails.auto', ['user' => "wdw"], function ($m) {
+ //            $m->from('from@gmail.com', 'Your Application');
+
+ //            $m->to('slavensakacic@gmail.com', "Slaven Sakačić")->subject('Your Reminder!');
+ //        });
+
+    return view('index');
+});
+
+Route::post('contactengine', ['as' => 'contactengine', function () {
+	$lorem= Input::all();
+   return view('contactengine')->with($lorem);
+}]);
---
+++ resources/views/footer.blade.php
@@ -0,0 +1,25 @@
+ <footer>
+        <div class="container">
+            <div class="social text-center">
+                <a href="https://twitter.com/slavensaka"><i class="fa fa-twitter"></i></a>
+                <a href="https://www.facebook.com/sakacic"><i class="fa fa-facebook"></i></a>
+                <a href="https://github.com/slavensaka"><i class="fa fa-github"></i></a>
+                <a href="https://hr.linkedin.com/pub/slaven-sakačić/a4/459/7ba"><i class="fa fa-linkedin"></i></a>
+            </div>
+
+            <div class="clear"></div>
+            <!--CLEAR FLOATS-->
+        </div>
+    </footer>
+    <a href="#top" class="topHome"><i class="fa fa-chevron-up fa-2x"></i></a>
+
+    <!--[if lte IE 8]><script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script><![endif]-->
+    <script src="js/modernizr-latest.js"></script>
+    <script src="js/jquery-1.8.2.min.js" type="text/javascript"></script>
+    <script src="js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="js/jquery.isotope.min.js" type="text/javascript"></script>
+    <script src="js/fancybox/jquery.fancybox.pack.js" type="text/javascript"></script>
+    <script src="js/jquery.nav.js" type="text/javascript"></script>
+    <script src="js/jquery.cslider.js" type="text/javascript"></script>
+    <script src="js/custom.js" type="text/javascript"></script>
+    <script src="js/owl-carousel/owl.carousel.js"></script>
